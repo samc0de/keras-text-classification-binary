bc11ebc5ea6447a8ed520fb19706b36c983e449b
sam.gofc <smahabole@google.com>
2018-04-09 17:52:43 +0530

    Minor fix: Use with for file opening.

--- classify.py
+++ classify.py
@@ -29,9 +29,9 @@ def convert_text_to_index_array(text):
 
 # TODO: Move these models to a separate dir.
 # read in your saved model structure
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
+with open('model.json', 'r') as json_file:
+    loaded_model_json = json_file.read()
+
 # and create a model from that
 model = model_from_json(loaded_model_json)
 # and weight your nodes with your saved values
