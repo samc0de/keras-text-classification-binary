829cf125d089816a868b649bef706947517c6360
sam.gofc <smahabole@google.com>
2018-04-09 18:07:24 +0530

    Create models for tensorflow serve by exporting keras models.
    Check if models have changed, they shouldn't.
    Attempts from previous commit/commented code doesn't work, it's from
    older versions of tensorflow.

--- tensorflow_serve_modeller.py
+++ tensorflow_serve_modeller.py
@@ -1,9 +1,16 @@
 import json
+
 import tensorflow as tf
+from tensorflow.python.saved_model import builder as saved_model_builder
+from tensorflow.python.saved_model import utils
+from tensorflow.python.saved_model import tag_constants, signature_constants
+from tensorflow.python.saved_model.signature_def_utils_impl import build_signature_def, predict_signature_def
 from tensorflow.contrib.session_bundle import exporter
+
+import keras
 from keras.models import model_from_json
 
-export_path = 'models/tensorflow_serve/'
+export_path = 'models/tensorflow_serve/exported'
 export_version = 0
 
 def main():
@@ -15,15 +22,26 @@ def main():
   # and weight your nodes with your saved values
   model.load_weights('model.h5')
 
-  saver = tf.train.Saver(sharded=True)
-  model_exporter = exporter.Exporter(saver)
-  signature = exporter.classification_signature(input_tensor=model.input,
-                                                scores_tensor=model.output)
-  with tf.Session() as sess:
-    model_exporter.init(sess.graph.as_graph_def(),
-                        default_graph_signature=signature)
-    model_exporter.export(export_path, tf.constant(export_version), sess)
+#   saver = tf.train.Saver(sharded=True)
+#   model_exporter = exporter.Exporter(saver)
+#   signature = exporter.classification_signature(input_tensor=model.input,
+#                                                 scores_tensor=model.output)
+#   with keras.backend.get_session() as sess:
+#     model_exporter.init(sess.graph.as_graph_def(),
+#                         default_graph_signature=signature)
+#     model_exporter.export(export_path, tf.constant(export_version), sess)
+#
 
+  builder = saved_model_builder.SavedModelBuilder(export_path)
+
+  signature = predict_signature_def(inputs={'images': model.input},
+                                    outputs={'scores': model.output})
+
+  with keras.backend.get_session() as sess:
+      builder.add_meta_graph_and_variables(
+          sess=sess, tags=[tag_constants.SERVING], signature_def_map={
+              'predict': signature})
+      builder.save()
 
 if __name__ == '__main__':
   main()
